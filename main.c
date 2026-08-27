438a4f72a43f9eddfda13841ebb33897eb255757
Manex Aranzadi <manexae@protonmail.com>
2026-08-27 16:41:07 +0200

    generado la structura

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 #include "escuchar.h"
 #include "identificar.h"
 
+#include "main.h"
 
 int main(int argc, char **argv) {
 
     if (argc>1)
     {
+
+        Usuario listaUsuario[104]; 
+
+
         if (strcmp(argv[1],"-e")==0)
         {
             escuchar();
--- main.h
+++ main.h
@@ -1,1 +1,7 @@
 #define PORT 5001
+
+
+typedef struct {
+    char ip[INET_ADDRSTRLEN];
+    char name[65];
+} Usuario;
