1e1f962edd7eaeb88f02f2f09a6c3cd28f2473d5
Manex Aranzadi <manexae@protonmail.com>
2026-08-31 09:29:54 +0200

    para que sea mas facil de distinguirlos

--- main.h
+++ main.h
@@ -14,9 +14,9 @@ void* threadEscuchar();
 void* threadIdentificar();
 
 
-#define DISCOVER 0x01
-#define CHAT 0x02
-#define FILE 0x04
+#define _DISCOVER 0x01
+#define _CHAT 0x02
+#define _FILE 0x04
 
 typedef struct {
     uint16_t id;
